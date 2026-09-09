45c7d2d6ef9316af6211686dc9b9bad7449be63b
alexselivpy <alexselivf@gmail.com>
2026-09-09 21:12:49 +0700

    Исправлены замечания для задачи Range

--- range_task/range.py
+++ range_task/range.py
@@ -1,26 +1,27 @@
 class Range:
-    def __init__(self, start: float, end: float):
+    def __init__(self, start: float, end: float) -> None:
         self.__start = start
         self.__end = end
 
     @property
-    def start(self):
+    def start(self) -> float:
         return self.__start
 
     @property
-    def end(self):
+    def end(self) -> float:
         return self.__end
 
+    @property
+    def length(self) -> float:
+        return self.__end - self.__start
+
     @start.setter
-    def start(self, start):
+    def start(self, start: float) -> None:
         self.__start = start
 
     @end.setter
-    def end(self, end):
+    def end(self, end: float) -> None:
         self.__end = end
 
-    def get_len(self):
-        return self.__end - self.__start
-
-    def is_inside(self, number):
+    def is_inside(self, number: float) -> bool:
         return self.__start <= number <= self.__end
--- range_task/range_program.py
+++ range_task/range_program.py
@@ -2,10 +2,13 @@ from range_task.range import Range
 
 user_start = float(input("Введите число - начало диапазона: "))
 user_end = float(input("Введите число - конец диапазона: "))
+
 user_range = Range(user_start, user_end)
-print(f"Длина диапазона = {user_range.get_len()}")
+print(f"Длина диапазона = {user_range.length}")
+
 user_number = float(input("Введите число, чтобы проверить, лежит ли число в данном диапазоне: "))
-if user_range.is_inside(user_number) is True:
+
+if user_range.is_inside(user_number):
     print(f"Число {user_number} лежит в диапазоне ({user_start},{user_end})")
 else:
     print(f"Число {user_number} не лежит в диапазоне ({user_start},{user_end})")
